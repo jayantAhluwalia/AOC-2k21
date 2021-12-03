d7ba35336c6c51405284fed424491c4101f11c61
jayantAhluwalia <jayantahluwalia07@gmail.com>
2021-12-04 01:08:27 +0530

    day1 part 1 :alien:

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 <h1> ADVENT OF CODE 2021 </h1>
+<p style="color: red">Trying to solve problems in functional programming way</p>
---
+++ day1/day1_part1.py
@@ -0,0 +1,18 @@
+with open("./input/inputs.txt", "r") as file:
+    measurements = [int(i.strip()) for i in file.readlines()]
+
+#print(measurements)
+# FUNCTION TO CHECK IF INCREASED
+def if_increased(pair):
+    return pair[1] > pair[0]
+
+# FUNCTION TO GET PAIRS
+def makePairs(measurements):
+    return list(zip(measurements, measurements[1:]))
+
+def solution(measurements):
+    return sum([if_increased(pair) for pair in makePairs(measurements)])
+
+print(solution(measurements))
+# doing the same thing
+#print(sum(x < y for x, y in zip(measurements, measurements[1:])))
